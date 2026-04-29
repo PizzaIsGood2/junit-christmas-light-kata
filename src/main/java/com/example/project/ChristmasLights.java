9f13d1075fb6f6d91bcfa2efb5b5fb8d180d1d2e
PizzaIsGood2 <jp630436@wne.edu>
2026-04-29 21:12:11 +0000

    test: added neighour lights test

--- src/main/java/com/example/project/ChristmasLights.java
+++ src/main/java/com/example/project/ChristmasLights.java
@@ -10,10 +10,6 @@
 
 package com.example.project;
 
-public class Calculator {
-
-	public int add(int a, int b) {
-		return a + b;
-	}
+public class ChristmasLights {
 
 }
--- src/test/java/com/example/project/ChristmasLightsTests.java
+++ src/test/java/com/example/project/ChristmasLightsTests.java
@@ -17,25 +17,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class CalculatorTests {
+class ChristmasLightsTests {
 
 	@Test
-	@DisplayName("1 + 1 = 2")
-	void addsTwoNumbers() {
-		Calculator calculator = new Calculator();
-		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
-	}
-
-	@ParameterizedTest(name = "{0} + {1} = {2}")
-	@CsvSource({
-			"0,    1,   1",
-			"1,    2,   3",
-			"49,  51, 100",
-			"1,  100, 101"
-	})
-	void add(int first, int second, int expectedResult) {
-		Calculator calculator = new Calculator();
-		assertEquals(expectedResult, calculator.add(first, second),
-				() -> first + " + " + second + " should equal " + expectedResult);
+	void testNeighbourPoints() {
+		assertEquals(2, ChristmasLights.TurnOn(0, 0, 0, 1));
 	}
 }
